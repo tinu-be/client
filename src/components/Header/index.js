4893b4a29924874c612b7429de34742eb5e7907c
Bruno Rodrigues <brunowebr@gmail.com>
2020-02-03 22:45:35 -0300

    Add label for 'beta'

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header() {
     return(
         <>
             <header id="header">
-                <img src={logo} alt="tinu.be - encurtador de url" width="160"/> 
+                <img src={logo} alt="tinu.be - encurtador de url" width="160"/> <span className="logo-label">beta</span>
 
                 <h1>Encurtador de urls</h1>
                 <p>Encurte links e compartilhe facilmente em qualquer lugar e com quem quiser.</p>
