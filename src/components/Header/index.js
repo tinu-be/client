02bea04d1636f97ea7b5c1b598b76d8135aa5cfb
Bruno Rodrigues <brunowebr@gmail.com>
2020-02-03 21:52:08 -0300

    Update logo and favicon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header() {
     return(
         <>
             <header id="header">
-                <img src={logo} alt="tinu.be - encurtador de url" width="120"/> 
+                <img src={logo} alt="tinu.be - encurtador de url" width="160"/> 
 
                 <h1>Encurtador de urls</h1>
                 <p>Use para encurtar urls compartilhar em seu Instagram, Facebook, WhatsApp e muito mais...</p>
