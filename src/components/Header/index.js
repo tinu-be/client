2c9c761fda831a882a3a8be00a6aa7400a837d3a
Bruno Rodrigues <brunowebr@gmail.com>
2020-02-03 21:57:38 -0300

    Update copy and inputs/button size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ function Header() {
                 <img src={logo} alt="tinu.be - encurtador de url" width="160"/> 
 
                 <h1>Encurtador de urls</h1>
-                <p>Use para encurtar urls compartilhar em seu Instagram, Facebook, WhatsApp e muito mais...</p>
+                <p>Encurte links e compartilhe em seu Instagram, Facebook, WhatsApp...</p>
             </header>
         </>
     )
